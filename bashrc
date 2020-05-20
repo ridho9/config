a3926a7ce1627ea946e3d59c87107320181e1177
Ridho Pratama <p.ridho@yahoo.co.id>
2020-05-21 02:17:11 +0700

    update

--- bashrc
+++ bashrc
@@ -2,11 +2,13 @@
 # ~/.bashrc
 #
 
-# If not running interactively, don't do anything
-[[ $- != *i* ]] && return
+source /opt/asdf-vm/asdf.sh
 
 export BROWSER=firefox
 export EDITOR=code
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
 
 if [[ $(ps --no-header --pid=$PPID --format=cmd) != "fish" ]]
 then
--- fish/config.fish
+++ fish/config.fish
@@ -31,26 +31,15 @@ set -g pure_color_git_branch (set_color brblue)
 
 # set -x EDITOR code
 # set -x BROWSER firefox-developer-edition
-set -x N_PREFIX ~/n
 
 set -x CONFIG ~/Project/config
 set -x PIPENV_VENV_IN_PROJECT 1
 
-set -x PATH ~/.yarn/bin ~/n/bin ~/.local/bin $PATH
-
-set -x PATH /home/rid9/perl5/bin $PATH ^/dev/null
-set -q PERL5LIB; and set -x PERL5LIB /home/rid9/perl5/lib/perl5:$PERL5LIB
-set -q PERL5LIB; or set -x PERL5LIB /home/rid9/perl5/lib/perl5
-set -q PERL_LOCAL_LIB_ROOT; and set -x PERL_LOCAL_LIB_ROOT /home/rid9/perl5:$PERL_LOCAL_LIB_ROOT
-set -q PERL_LOCAL_LIB_ROOT; or set -x PERL_LOCAL_LIB_ROOT /home/rid9/perl5
-set -x PERL_MB_OPT '--install_base "/home/rid9/perl5"'
-set -x PERL_MM_OPT INSTALL_BASE=/home/rid9/perl5
+set -x PATH ~/.local/bin $PATH
 
 set -x PATH ~/Android/Sdk/platform-tools $PATH
-set -x PATH ~/.nimble/bin $PATH
-set -x PATH /home/rid9/rakudo/install/bin /home/rid9/rakudo/install/share/perl6/site/bin $PATH
-
-set -x JAVA_HOME /usr/lib/jvm/java-8-openjdk
+# set -x PATH ~/.nimble/bin $PATH
+# set -x PATH /home/rid9/rakudo/install/bin /home/rid9/rakudo/install/share/perl6/site/bin $PATH
 
 set -x GOPATH ~/go
 
@@ -58,16 +47,6 @@ direnv hook fish | source
 hub alias -s | source
 source /opt/asdf-vm/asdf.fish
 
-# # opam configuration
-# source /home/rid9/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
-
-source /home/rid9/.config/fish/secret.fish
+source $CONFIG/fish/secret.fish
 
 set -x MANPATH /usr/share/man $MANPATH
-
-# Load rbenv automatically by appending
-# the following to ~/.config/fish/config.fish:
-
-# status --is-interactive; and source (rbenv init -|psub)
-
-# starship init fish | source
